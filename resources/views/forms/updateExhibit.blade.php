09444debbbe8e251fd725d4d5e20632f76ab9bd3
RichLim <richlim16@gmail.com>
2021-05-29 11:55:56 +0800

    added checked value for checkboxes in exhibit gallery selection

--- resources/views/forms/updateExhibit.blade.php
+++ resources/views/forms/updateExhibit.blade.php
@@ -15,7 +15,7 @@
             <label for="art-{{$art['id']}}">
                 <div class="card hover">
                     <div class="title">
-                        <input style="float:left; " id="art-{{$art['id']}}" name="art[]" type="checkbox" value="{{$art['id']}}">
+                        <input style="float:left; " id="art-{{$art['id']}}" name="art[]" type="checkbox" value="{{$art['id']}}" @if($art->exhibit_id == $id) checked  @endif>
                         {{$art['title']}}
                     </div>
                     <img src="{{asset('storage/art/'.$art->photo)}}" alt="Art Photo" style="height: 100px; width: 50px; object-fit: cover;" id="img">
@@ -40,7 +40,7 @@
             <label for="poetry-{{$poetry['id']}}">
                 <div class="poetry-card hover">
                     <div class="title">
-                        <input style="float:left;" id="poetry-{{$poetry['id']}}" name="poetry[]"type="checkbox" value="{{$poetry['id']}}">
+                        <input style="float:left;" id="poetry-{{$poetry['id']}}" name="poetry[]"type="checkbox" value="{{$poetry['id']}}" @if($poetry->exhibit_id == $id) checked  @endif>
                         "{{$poetry['title']}}"
                     </div>
                     <div style="background-color: #1e3c86; overflow-y:scroll; padding: 15px 25px">
